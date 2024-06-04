7ba7557d6c56f4f1b3f93be5d10af3a1a91ce594
Albert <albert.bernadas.fabra@gmail.com>
2024-06-04 13:07:35 +0200

    Agregar archivos de proyecto.

---
+++ Equipo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Equipo
+{
+	public Equipo()
+	{
+		int Id { get; set; }
+		string Nombre { get; set; }
+		List<Jugador> jugadores;
+		public Equipo(int id, string nombre) 
+		{
+			Id = id;
+			Nombre = nombre;
+			List < Jugador > = new List<Jugador>();
+		}
+        public void AddJugador(Jugador jugador)
+        {
+            jugadores.Add(jugador);
+		}
+		public string ViewJugadores()
+		{
+
+		}
+    }
+
+}
