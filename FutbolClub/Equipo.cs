69258d3b5cf2f3f7c82534714806b5f813d93b61
Albert <albert.bernadas.fabra@gmail.com>
2024-06-04 13:19:26 +0200

    Correccion de errores

--- FutbolClub/Equipo.cs
+++ FutbolClub/Equipo.cs
@@ -1,31 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 
-public class Equipo
+internal class Equipo
 {
-	public Equipo()
+	int Id { get; set; }
+	string Nombre { get; set; }
+	List<Jugador> jugadores;
+	public Equipo(int id, string nombre) 
 	{
-		int Id { get; set; }
-		string Nombre { get; set; }
-		List<Jugador> jugadores;
-		public Equipo(int id, string nombre) 
+		Id = id;
+		Nombre = nombre;
+		List < Jugador > = new List<Jugador>();
+	}
+    public void AddJugador(Jugador jugador)
+    {
+        jugadores.Add(jugador);
+	}
+	public string ViewJugadores()
+	{
+		string salida = "";
+		foreach (Jugador jugador in jugadores)
 		{
-			Id = id;
-			Nombre = nombre;
-			List < Jugador > = new List<Jugador>();
+			salida += jugador.ToString();
 		}
-        public void AddJugador(Jugador jugador)
-        {
-            jugadores.Add(jugador);
-		}
-		public string ViewJugadores()
-		{
-			string salida = "";
-			foreach (Jugador jugador in jugadores)
-			{
-				salida += jugador.ToString();
-			} 
-		}
-    }
+		return salida;
+	}
 
 }
