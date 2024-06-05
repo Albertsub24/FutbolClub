5888da2f01b56813a0ee2b923848c496a7a81bda
Cath Cardenas <cathcardenasm@gmail.com>
2024-06-05 10:23:11 +0200

    Clase Club modificada. Metodos añadidos: AgregarJugador AsignarJugadorAEquipo ListarJugadoresDeEquipo

--- FutbolClub/Club.cs
+++ FutbolClub/Club.cs
@@ -1,5 +1,4 @@
 ﻿// ClubFutbol/Club.cs
-
 using System;
 using System.Collections.Generic;
 
@@ -8,10 +7,12 @@ namespace FutbolClub
     class Club
     {
         private List<Equipo> equipos;
+        private List<Jugador> jugadores;
 
         public Club()
         {
             equipos = new List<Equipo>();
+            jugadores = new List<Jugador>();
         }
 
         public void AgregarEquipo(Equipo equipo)
@@ -24,6 +25,41 @@ namespace FutbolClub
             foreach (var equipo in equipos)
             {
                 Console.WriteLine(equipo.Nombre);
+            }
+        }
+
+        public void AgregarJugador(Jugador jugador)
+        {
+            jugadores.Add(jugador);
+        }
+
+        public void AsignarJugadorAEquipo(int numeroJugador, string nombreEquipo)
+        {
+            Jugador jugador = jugadores.Find(j => j.NumeroCamiseta == numeroJugador);
+            Equipo equipo = equipos.Find(e => e.Nombre == nombreEquipo);
+
+            if (jugador != null && equipo != null)
+            {
+                equipo.AddJugador(jugador);
+                Console.WriteLine($"Jugador {jugador.Nombre} asignado al equipo {equipo.Nombre}");
+            }
+            else
+            {
+                Console.WriteLine("Jugador o equipo no encontrado");
+            }
+        }
+
+        public void ListarJugadoresDeEquipo(string nombreEquipo)
+        {
+            Equipo equipo = equipos.Find(e => e.Nombre == nombreEquipo);
+
+            if (equipo != null)
+            {
+                equipo.ViewJugadores();
+            }
+            else
+            {
+                Console.WriteLine("Equipo no encontrado");
             }
         }
     }
