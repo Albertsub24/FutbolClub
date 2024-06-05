1ab15bd9fbb3641c254291faf1d70c8b3bef622c
Cath Cardenas <cathcardenasm@gmail.com>
2024-06-05 09:52:51 +0200

    Clase Club creada. Metodos: AgregarEquipo, ListarEquipos añadidas

---
+++ FutbolClub/Club.cs
@@ -0,0 +1,29 @@
+﻿// ClubFutbol/Club.cs
+using System;
+using System.Collections.Generic;
+
+namespace ClubFutbol
+{
+    class Club
+    {
+        private List<Equipo> equipos;
+
+        public Club()
+        {
+            equipos = new List<Equipo>();
+        }
+
+        public void AgregarEquipo(Equipo equipo)
+        {
+            equipos.Add(equipo);
+        }
+
+        public void ListarEquipos()
+        {
+            foreach (var equipo in equipos)
+            {
+                Console.WriteLine(equipo.Nombre);
+            }
+        }
+    }
+}
