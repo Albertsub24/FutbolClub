e12ba93ae9a1f05a978135158d79bc79edbfe877
Cath Cardenas <cathcardenasm@gmail.com>
2024-06-06 09:43:00 +0200

    Clase Equipo modificada

--- FutbolClub/Club.cs
+++ FutbolClub/Club.cs
@@ -8,14 +8,8 @@ namespace FutbolClub
 {
     class Club
     {
-        private List<Equipo> equipos;
-        private List<Jugador> jugadores;
-
-        public Club()
-        {
-            equipos = new List<Equipo>();
-            jugadores = new List<Jugador>();
-        }
+        private List<Jugador> jugadores = new List<Jugador>();
+        private List<Equipo> equipos = new List<Equipo>();
 
         public void AgregarEquipo(Equipo equipo)
         {
@@ -37,8 +31,8 @@ namespace FutbolClub
 
         public void AsignarJugadorAEquipo(int numeroJugador, string nombreEquipo)
         {
-            Jugador jugador = jugadores.Find(j => j.NumeroCamiseta == numeroJugador);
-            Equipo equipo = equipos.Find(e => e.Nombre == nombreEquipo);
+            Jugador jugador = jugadores.FirstOrDefault(j => j.NumeroCamiseta == numeroJugador);
+            Equipo equipo = equipos.FirstOrDefault(e => e.Nombre == nombreEquipo);
 
             if (jugador != null && equipo != null)
             {
@@ -62,6 +56,58 @@ namespace FutbolClub
             else
             {
                 Console.WriteLine("Equipo no encontrado");
+            }
+        }
+
+        public void Menu()
+        {
+            while (true)
+            {
+                Console.WriteLine("1. Crear equipo");
+                Console.WriteLine("2. Listar equipos");
+                Console.WriteLine("3. Crear jugador");
+                Console.WriteLine("4. Asignar jugador a equipo");
+                Console.WriteLine("5. Listar jugadores de un equipo");
+                Console.WriteLine("6. Salir");
+                string opcion = Console.ReadLine();
+
+                switch (opcion)
+                {
+                    case "1":
+                        Console.Write("Nombre del equipo: ");
+                        string nombreEquipo = Console.ReadLine();
+                        AgregarEquipo(new Equipo(nombreEquipo));
+                        break;
+                    case "2":
+                        ListarEquipos();
+                        break;
+                    case "3":
+                        Console.Write("Nombre del jugador: ");
+                        string nombreJugador = Console.ReadLine();
+                        Console.Write("Posición del jugador: ");
+                        string posicionJugador = Console.ReadLine();
+                        Console.Write("Número del jugador: ");
+                        int numeroJugador = int.Parse(Console.ReadLine());
+                        AgregarJugador(new Jugador(nombreJugador, posicionJugador, numeroJugador));
+                        break;
+                    case "4":
+                        Console.Write("Nombre del equipo: ");
+                        string equipoParaAsignar = Console.ReadLine();
+                        Console.Write("Número del jugador: ");
+                        int numeroParaAsignar = int.Parse(Console.ReadLine());
+                        AsignarJugadorAEquipo(numeroParaAsignar, equipoParaAsignar);
+                        break;
+                    case "5":
+                        Console.Write("Nombre del equipo: ");
+                        string equipoParaListar = Console.ReadLine();
+                        ListarJugadoresDeEquipo(equipoParaListar);
+                        break;
+                    case "6":
+                        return;
+                    default:
+                        Console.WriteLine("Opción no válida");
+                        break;
+                }
             }
         }
     }
